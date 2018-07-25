9ec7a95c510f252059c7ed0089f51ceaed95da84
Juan David <juandavid92000@gmail.com>
2018-07-24 19:36:09 -0600

    24 julio 2018
    controladorProductoFaseElementoRango ahora es quien define cual sera el tipo de alerta  que se aplicara

--- app/Http/Controllers/MasterControler.php
+++ app/Http/Controllers/MasterControler.php
@@ -5,17 +5,19 @@ use App\Elementos;
 use App\UnidadDeMedida;
 use App\Estaciones;
 use App\ValoresElementos;
-
+use Carbon\Carbon;
 class MasterControler extends Controller
 {
 /*
                                        PUNTOS IMPORANTES POR PROGRAMAR
-1.Definir en que fase fenologica del año estamos
+1.Definir en que fase fenologica del año estamos *OK*
 2.Sincronizar automaticamente tablas generales *OK*
 3.Determinar si  una jornada Diurna o Nocturna en base a la hora del dia *OK*
 4.Registro de Valores elementos.*OK*
 5.Registro de Alertas.
 6.La validacion de precipitacion del cuadro de excel de capucas debe realizar en base a acumulados
+7. definir seting para creacion de variables locales.
+8.Investigar e implementar elRolback en eloquent laravel
 */
   public function index()
   {
@@ -52,172 +54,159 @@ foreach ($estaciones as $estacion) {
      $controlador =null;
      $controlador = new ConsumirApiDeCICOHControler();
      $items = $controlador->DatosDeUltimaHora();
-     //Obener información de la tabla ProductoFaseElementoRango
-     $controladorProductoFaseElementoRango =null;
-     $controladorProductoFaseElementoRango =  new ProductoFaseElementoRangoControler();
-     $productoFaseElementoRangos =$controladorProductoFaseElementoRango->all();
-     $array = json_decode($productoFaseElementoRangos,true);
-     foreach($items ['resource']  as $item ) {
-       //Verificar el registro de la estacion.
-       $exixteLaEstacion=false;
-        $estacionesControler= new EstacionesControler();
-        $exixteLaEstacion= $estacionesControler->VerificarExistencia($item['station_id'] );
-       if($exixteLaEstacion == false)
-       {
+     //Tipo de Produtos.
+     $tipoDeProductoControler =null;
+     $tipoDeProductoControler = new TipoDeProductoControler();
+     $tipoDeProducto =$tipoDeProductoControler->ObtenerTipoProductosActivos();
+     echo "----------------TipoDeProducto--------------------".'<br>';
+     $valoreselementos_id=0;
+     foreach ($tipoDeProducto as $tipoProducto) {
+             $valor=  $tipoProducto['id'];
+             echo ' ---------       '.$valor.'<br>';
+       //Fase fenologica.
+        $fechaActual =Carbon::now();
+       $mesActual =date("m",strtotime($fechaActual));
+       $faseFenologicaMesAlertaControler= new FaseFenologicaMesAlertaControler();
+       $faseFenologicaMesAlerta =$faseFenologicaMesAlertaControler->ObtenerParametrosDeFaseFenologicaMesAlerta($mesActual,$tipoProducto['id']);
+       foreach ($faseFenologicaMesAlerta as $fase) {
+       $valor=  $fase['fasefenologica_id'];
+       echo ' ---------Fases Fenologica       '.$valor.'<br>';
 
-         $nuevaEsacion= new Estaciones();
-         $nuevaEsacion->id=$item['station_id'];
-         $nuevaEsacion->descripcion = $item['station_name'];
-         $nuevaEsacion->latitud =$item['latitude'];
-         $nuevaEsacion->longitud =$item['longitude'];
-         $nuevaEsacion->elevacion =$item['elevation'];
-         $nuevaEsacion->estaactivo = true;
-         $nuevaEsacion->id_usuariocreo = 1;
-         $nuevaEsacion->save();
-         $exixteLaEstacion =true;
+              foreach($items ['resource']  as $item ) {
+                             //tiempo de estacionenes.
+                             $fechaDeEstacion=   $item['datetime'];
+                              $hora =date("H",strtotime($fechaDeEstacion));
+                             $dia =date("d",strtotime($fechaDeEstacion));
+                             $mes =date("m",strtotime($fechaDeEstacion));
+                             $anio =date("y",strtotime($fechaDeEstacion));
+                             //Verificar el registro de la estacion.
+                             $exixteLaEstacion=false;
+                              $estacionesControler= new EstacionesControler();
+                              $exixteLaEstacion= $estacionesControler->VerificarExistencia($item['station_id'] );
+                             if($exixteLaEstacion == false)
+                             {
 
-       }
+                               $nuevaEsacion= new Estaciones();
+                               $nuevaEsacion->id=$item['station_id'];
+                               $nuevaEsacion->descripcion = $item['station_name'];
+                               $nuevaEsacion->latitud =$item['latitude'];
+                               $nuevaEsacion->longitud =$item['longitude'];
+                               $nuevaEsacion->elevacion =$item['elevation'];
+                               $nuevaEsacion->estaactivo = true;
+                               $nuevaEsacion->id_usuariocreo = 1;
+                               $nuevaEsacion->save();
+                               $exixteLaEstacion =true;
 
-       //Verificar el registro de unidades de UnidadDeMedida
-          $existeUnidadDeMedida = $unidadDeMedidaControler->VerificarExistencia($item['symbol'] );
-                  if(!$existeUnidadDeMedida)
-                  {
-               $nuevaUnidadDeMedida = new UnidadDeMedida();
-               $nuevaUnidadDeMedida->simbolo = $item['symbol'];
-               $nuevaUnidadDeMedida->descripcion = $item['symbol'];
-               $nuevaUnidadDeMedida->estaactivo = true;
-               $nuevaUnidadDeMedida->id_usuariocreo = 1;
-               $nuevaUnidadDeMedida->save();
-               $existeUnidadDeMedida =true;
-                  }
-       //verificar si esta registrado el elemento.
-        $existeElemento=  $elementosControlador->VerificarExistencia( $item['element_id'] );
-       if(!$existeElemento)
-       {
-         $nuevoElemento =  new Elementos;
-         $nuevoElemento->id = $item['element_id'];
-         $nuevoElemento->simbolo = $item['symbol'];
-         $nuevoElemento->descripcion = $item['element_symbol']." ".$item['element_name'];
-         $nuevoElemento->estaactivo = true;
-         $nuevoElemento->id_usuariocreo = 1;
-        $nuevoElemento->save();
-        $existeElemento =true;
-       }
-       //tiempo de estacionenes.
-       $fechaDeEstacion=   $item['datetime'];
-        $hora =date("H",strtotime($fechaDeEstacion));
-       $dia =date("d",strtotime($fechaDeEstacion));
-       $mes =date("m",strtotime($fechaDeEstacion));
-       $anio =date("y",strtotime($fechaDeEstacion));
-//Verificacion de existencia de ValoresElementos.
-$listaDeValoresElementos = array([]);
-$listaDeValoresElementos = array_add($listaDeValoresElementos, 'estaciones_id', $item['station_id']);
-$listaDeValoresElementos = array_add($listaDeValoresElementos, 'elementos_id', $item['element_id']);
-$listaDeValoresElementos = array_add($listaDeValoresElementos, 'unidaddemedida_simbolo', $item['symbol']);
-$listaDeValoresElementos = array_add($listaDeValoresElementos, 'fechaestacion', $item['datetime']);
-$listaDeValoresElementos = array_add($listaDeValoresElementos, 'valor', $item['valor']);
-$listaDeValoresElementos = array_add($listaDeValoresElementos, 'estaactivo', true);
-$listaDeValoresElementos = array_add($listaDeValoresElementos, 'dia', $dia);
-$listaDeValoresElementos = array_add($listaDeValoresElementos, 'mes', $mes);
-$listaDeValoresElementos = array_add($listaDeValoresElementos, 'anio', $anio);
-$valoresElementosControler= new ValoresElementosControler();
-$existenValoresElementos = $valoresElementosControler->VerificarExistencia($listaDeValoresElementos);
-//Si no esta registrado en la base de datos se registra
-if($existenValoresElementos != 1)
-{
-  $valoresElementosControler->store($listaDeValoresElementos);
+                             }
+
+                             //Verificar el registro de unidades de UnidadDeMedida
+                                $existeUnidadDeMedida = $unidadDeMedidaControler->VerificarExistencia($item['symbol'] );
+                                        if(!$existeUnidadDeMedida)
+                                        {
+                                     $nuevaUnidadDeMedida = new UnidadDeMedida();
+                                     $nuevaUnidadDeMedida->simbolo = $item['symbol'];
+                                     $nuevaUnidadDeMedida->descripcion = $item['symbol'];
+                                     $nuevaUnidadDeMedida->estaactivo = true;
+                                     $nuevaUnidadDeMedida->id_usuariocreo = 1;
+                                     $nuevaUnidadDeMedida->save();
+                                     $existeUnidadDeMedida =true;
+                                        }
+                             //verificar si esta registrado el elemento.
+                              $existeElemento=  $elementosControlador->VerificarExistencia( $item['element_id'] );
+                             if(!$existeElemento)
+                             {
+                               $nuevoElemento =  new Elementos;
+                               $nuevoElemento->id = $item['element_id'];
+                               $nuevoElemento->simbolo = $item['symbol'];
+                               $nuevoElemento->descripcion = $item['element_symbol']." ".$item['element_name'];
+                               $nuevoElemento->estaactivo = true;
+                               $nuevoElemento->id_usuariocreo = 1;
+                              $nuevoElemento->save();
+                              $existeElemento =true;
+                             }
+
+                      //Verificacion de existencia de ValoresElementos.
+                      $listaDeValoresElementos = array([]);
+                      $listaDeValoresElementos = array_add($listaDeValoresElementos, 'estaciones_id', $item['station_id']);
+                      $listaDeValoresElementos = array_add($listaDeValoresElementos, 'elementos_id', $item['element_id']);
+                      $listaDeValoresElementos = array_add($listaDeValoresElementos, 'unidaddemedida_simbolo', $item['symbol']);
+                      $listaDeValoresElementos = array_add($listaDeValoresElementos, 'fechaestacion', $item['datetime']);
+                      $listaDeValoresElementos = array_add($listaDeValoresElementos, 'valor', $item['valor']);
+                      $listaDeValoresElementos = array_add($listaDeValoresElementos, 'estaactivo', true);
+                      $listaDeValoresElementos = array_add($listaDeValoresElementos, 'dia', $dia);
+                      $listaDeValoresElementos = array_add($listaDeValoresElementos, 'mes', $mes);
+                      $listaDeValoresElementos = array_add($listaDeValoresElementos, 'anio', $anio);
+                      $valoresElementosControler= new ValoresElementosControler();
+                      $existenValoresElementos = $valoresElementosControler->VerificarExistencia($listaDeValoresElementos);
+                      //Si no esta registrado en la base de datos se registra
+
+                    if($existenValoresElementos != 1)
+                      {
+                      $valoreselementos_id =  $valoresElementosControler->store($listaDeValoresElementos);
+
+                      }
+                      //ObtenerParametrosDeProductoFaseElementoRango
+                      //identificacion de jornada
+                      $jornada =0;
+                      if( $hora >=5  and $hora <=17 )
+                      {
+                        $jornada =1;
+                      }
+                      else {
+                        $jornada =2;
+                      }
+                      //Obener información de la tabla ProductoFaseElementoRango
+                      $productoFaseElementoRangosfiltro = array([]);
+                      $productoFaseElementoRangosfiltro = array_add($productoFaseElementoRangosfiltro, 'tipoproducto_id', $tipoProducto['id']);
+                      $productoFaseElementoRangosfiltro = array_add($productoFaseElementoRangosfiltro, 'jornada_id', $jornada);
+                      $productoFaseElementoRangosfiltro = array_add($productoFaseElementoRangosfiltro, 'fasefenologica_id', $fase['fasefenologica_id']);
+                      $controladorProductoFaseElementoRango =null;
+                      $controladorProductoFaseElementoRango =  new ProductoFaseElementoRangoControler();
+                      $productoFaseElementoRangos = $controladorProductoFaseElementoRango->ObtenerParametrosDeProductoFaseElementoRango($productoFaseElementoRangosfiltro);
+
+
+                     foreach($productoFaseElementoRangos as $productoFaseElementoRango)
+                     {
+                   echo $valoreselementos_id;
+                      if($valoreselementos_id >0)//tiene que registrar nuevos valores elementos.
+                     {
+                       echo "ento al for de  productoFaseElementoRango 2 ".'</br>';
+                     if(  ($item['element_id']== $productoFaseElementoRango['elementos_id'])  )
+                     {
+                       if($item['valor']< $productoFaseElementoRango['valorminimo'])
+                       {
+                        echo "ento al for de  productoFaseElementoRango 3 ".'</br>';
+
+                           $estacionesAlertasValores = array([]);
+                           $estacionesAlertasValores =array_add($estacionesAlertasValores, 'valoreselementos_id', $valoreselementos_id);
+                           $estacionesAlertasValores =array_add($estacionesAlertasValores, 'tipodealerta_id', $productoFaseElementoRango['tipodealerta_id']);
+                           $estacionesAlertasControler = new EstacionesAlertasControler();
+                           $estacionesAlertas = $estacionesAlertasControler->store($estacionesAlertasValores);
+
+
+                         echo $item['valor'].$item['symbol']." "." Este valor es menor que el minimo permitido --".$productoFaseElementoRango['valorminimo']."--Codigo Elemento ".$productoFaseElementoRango['elementos_id'].'<br>';
+                       }
+                       if($item['valor']> $productoFaseElementoRango['valormaximo'])
+                       {
+                         echo $item['valor'].$item['symbol']." "." Este valor es mayor que el maximo permitido --".$productoFaseElementoRango['valormaximo']."--Codigo Elemento ".$productoFaseElementoRango['elementos_id'].'<br>';
+                       }
+                     }
+                    }
+
+                   }
+
 }
-//Registro de Alertas.
-$jornada =0;
-if( $hora >=5  and $hora <=17 )
-{
-  $jornada =1;
-}
-else {
-  $jornada =2;
-}
-//ObtenerParametrosDeProductoFaseElementoRango
+   }//fin Fase fenologica.
+}//Fin de tipo de productos
 
-         foreach($productoFaseElementoRangos as $productoFaseElementoRango)
-         {
-
-         if(  ($item['element_id']== $productoFaseElementoRango['elementos_id']) and $item['valor'] !=0  )
-         {
-           if($item['valor']< $productoFaseElementoRango['valorminimo'])
-           {
-             echo $item['valor'].$item['symbol']." "." Este valor es menor que el minimo permitido --".$productoFaseElementoRango['valorminimo']."--Codigo Elemento ".$productoFaseElementoRango['elementos_id'].'<br>';
-           }
-           if($item['valor']> $productoFaseElementoRango['valormaximo'])
-           {
-             echo $item['valor'].$item['symbol']." "." Esta valor es mayor que el maximo permitido --".$productoFaseElementoRango['valorminimo']."--Codigo Elemento ".$productoFaseElementoRango['elementos_id'].'<br>';
-           }
-         }
-       }
-
-}
-
-     foreach($items ['resource']  as $item ) {
-       //Inicialización de variables
-             $station_id  =0;
-             $station_name   ="";
-             $latitude  =0;
-             $longitude  =0;
-             $elevation   =0;
-             $element_id   =0;
-             $element_symbol ="";
-             $symbol   ="";
-             $element_name ="";
-             $valor =0;
-             $datetime="0000-00-00 00:00:00" ;
-             //Asignacion
-             $station_id  = $item['station_id'];
-             $station_name   = $item['station_name'];
-             $latitude  = $item['latitude'];
-             $longitude  = $item['longitude'];
-             $elevation   = $item['elevation'];
-             $element_id   = $item['element_id'];
-             $element_symbol   = $item['element_symbol'];
-             $symbol   = $item['symbol'];
-             $element_name = $item['element_name'];
-             $valor = $item['valor'];
-             $datetime = $item['datetime'];
-             $descripcion = $station_id." ".$station_name." Id Elemento ".$element_id." simbolo ".$element_symbol." ".$element_name." Valor ".$valor." Unidad ".$symbol ;
-           }
 
 return view('prueba');
   }
 
 
 
-  public function RegistroDeValoresElementos()
-  {
-            try {
-
-            } catch (\Exception $e) {
-
-            }
 
 
-   }
-  public function VerificacionDeAlertas ()
-  {
-    try {
-
-        } catch (\Exception $e) {
-
-        }
-
-  }
-
-    public function RegistroDeAlertas ()
-    {
-      try {
-
-          } catch (\Exception $e) {
-
-          }
-
-    }
 
 
 
